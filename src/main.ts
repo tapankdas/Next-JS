eb2e029d18d9e145bffb5dfef3bc46f9031fcc97
tapankdas <tapan.kdas@gmail.com>
2023-10-01 13:14:52 +0530

    first commit

--- src/main.ts
+++ src/main.ts
@@ -5,10 +5,12 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    credentials:true,
-    origin:"http://localhost:3000",
+    credentials:true,    
+    origin:["http://localhost:3000","https://main.d2ikncomdr6rnr.amplifyapp.com"],
   });
   
+  
+
   app.use(cookieParser())
   await app.listen(4000);
 }
